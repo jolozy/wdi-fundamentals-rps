496263751d8aed57293b27c0af51ec8fb5af3041
jolozy <joannelozy@outlook.com>
2016-01-13 11:31:20 +0800

    completed Unit 4 Homework

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -41,8 +41,24 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
     return winner;
+    if (playerMove === computerMove) {
+        winner = 'tie'
+    } else if (playermove === 'rock' || computerMove === 'scissors') {
+        winner = 'player'
+    } else if (playermove === 'scissors' || computerMove === 'paper') {
+        winner = 'player'
+    } else if (playerMove === 'paper' || computerMove === 'rock') {
+        winner = 'player'
+    } else if (computerMove === 'rock' || playermove === 'scissors') {
+        winner = 'computer'
+    } else if (computerMove === 'scissors' || playermove === 'paper') {
+        winner = 'computer'
+    } else if (computerMove === 'paper' || playermove === 'rock') {
+        winner = 'computer'
+    } else {
+        winner = 'invalid input'
+    }
 }
 
 function playToFive() {
